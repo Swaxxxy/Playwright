e667a10a95a3cf451d19386150b7886fad2e070e
Danil <dozorovda@yandex.ru>
2026-01-18 18:27:09 +0300

    Добавлен тест надо добавление товара в корзину.Выяснил, что кнопка добавить привязана к конкретной карточке

--- Saucedemo_com/Inventory_Page_Selectors.py
+++ Saucedemo_com/Inventory_Page_Selectors.py
@@ -4,10 +4,15 @@
 menu_button = '#react-burger-menu-btn'
 filter_button = '[data-test="product-sort-container"]'
 cart = '[data-test="shopping-cart-link"]'
+cart_badge = '[data-test="shopping-cart-badge"]'
+
 
 #card_selectors
 card_header = '[data-test="inventory-item-name"]'
 card_description = '[data-test="inventory-item-desc"]'
 card_price = '[data-test="inventory-item-price"]'
-card_add_button = '[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]'
 card_img = '[data-test="inventory-item-sauce-labs-bike-light-img"]'
+
+# это селектор для кнопки внутри КОНКРЕТНОЙ карточки
+card_add_button = '[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]'
+card_remove_button = '[data-test="remove-sauce-labs-bolt-t-shirt"]'
